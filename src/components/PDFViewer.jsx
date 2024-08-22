480ec271ee9c40278afbb839f795c1dc0764ea6c
Anthonyushie <anthonytwan75official@gmail.com>
2024-08-22 21:42:52 +0100

    Mobile View adjustment

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -5,11 +5,15 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import pdfFile from '../../public/Liturgy-of-songs.pdf';  // Adjust the path as necessary
 
 function PDFViewer() {
+    const defaultScale = window.innerWidth < 768 ? 0.8 : 1.7; // 1.0x zoom for mobile, 2.0x for desktop
   return (
     <div>
       <h1>PDF Viewer</h1>
       <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={pdfFile} />
+        <Viewer 
+        fileUrl={pdfFile} 
+        defaultScale={defaultScale}
+        />
       </Worker>
     </div>
   );
